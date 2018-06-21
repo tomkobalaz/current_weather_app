01b243a62db0eeb3b9541b63e6639a7362fb8530
Tomas Balaz <tomko@XCLBR.STAGING>
2018-06-21 14:38:18 +0200

    the end

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 let request = require('request');
+let argv = require('yargs').argv
 
 let apiKey = 'a3d5a9d815df7658071a71f6eeaddc21';
-let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let city = argv.c || 'poprad';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+      let weather = JSON.parse(body)
+      let message = `It's ${weather.main.temp} degrees in ${weather.name}.`;
+      console.log(message);
   }
 });
